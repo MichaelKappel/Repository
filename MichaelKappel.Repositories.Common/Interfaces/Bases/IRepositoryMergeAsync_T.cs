397444a61dc3daf4832bd4c0faf084934ad31a61
MichaelKappel <mike@ns12.com>
2023-11-16 06:53:40 -0600

    WIP: checkin

--- MichaelKappel.Repositories.Common/Interfaces/Bases/IRepositoryMergeAsync_T.cs
+++ MichaelKappel.Repositories.Common/Interfaces/Bases/IRepositoryMergeAsync_T.cs
@@ -7,6 +7,22 @@ namespace MichaelKappel.Repository.Interfaces.Bases
 {
     public interface IRepositoryMergeAsync<T, TUnsaved>
     {
-        Task<T> Merge(TUnsaved model);
+        Task<T> MergeAsync(TUnsaved model);
     }
+
+    public interface IRepositoryMergeAsync<TResult, TKey1, TKey2>
+    {
+        Task<TResult> MergeAsync(TKey1 key1, TKey2 key2);
+    }
+
+    public interface IRepositoryMergeAsync<TResult, TKey1, TKey2, TKey3>
+    {
+        Task<TResult> MergeAsync(TKey1 key1, TKey2 key2, TKey3 key3);
+    }
+
+    public interface IRepositoryMergeAsync<TResult, TKey1, TKey2, TKey3, TKey4>
+    {
+        Task<TResult> MergeAsync(TKey1 key1, TKey2 key2, TKey3 key3, TKey4 key4);
+    }
+
 }
--- MichaelKappel.Repositories.Common/Interfaces/Bases/IRepositoryMerge_T.cs
+++ MichaelKappel.Repositories.Common/Interfaces/Bases/IRepositoryMerge_T.cs
@@ -9,4 +9,20 @@ namespace MichaelKappel.Repository.Interfaces.Bases
     {
         T Merge(TUnsaved model);
     }
+
+    public interface IRepositoryMerge<TResult, TKey1, TKey2>
+    {
+        TResult Merge(TKey1 key1, TKey2 key2);
+    }
+
+    public interface IRepositoryMerge<TResult, TKey1, TKey2, TKey3>
+    {
+        TResult Merge(TKey1 key1, TKey2 key2, TKey3 key3);
+    }
+
+    public interface IRepositoryMerge<TResult, TKey1, TKey2, TKey3, TKey4>
+    {
+        TResult Merge(TKey1 key1, TKey2 key2, TKey3 key3, TKey4 key4);
+    }
+
 }
