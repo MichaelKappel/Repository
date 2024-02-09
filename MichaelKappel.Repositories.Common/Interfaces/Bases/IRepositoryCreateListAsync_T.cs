8d949e5ad4427eed6b4293cc76dfca3365feb41b
Michael Kappel <Michael.Kappel@Info724.onmicrosoft.com>
2024-02-09 08:41:56 -0600

    Added IRepositoryCreateList and IRepositoryCreateListAsync

---
+++ MichaelKappel.Repositories.Common/Interfaces/Bases/IRepositoryCreateListAsync_T.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MichaelKappel.Repository.Interfaces.Bases
+{
+    public interface IRepositoryCreateListAsync<TResult, T>
+    {
+        Task<IList<TResult>> CreateListAsync(IList<T> model);
+    }
+
+    public interface IRepositoryCreateListAsync<TResult, TKey1, TKey2>
+    {
+        Task<IList<TResult>> CreateListAsync(IList<TKey1> key1, TKey2 key2);
+    }
+}
---
+++ MichaelKappel.Repositories.Common/Interfaces/Bases/IRepositoryCreateList_T.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MichaelKappel.Repository.Interfaces.Bases
+{
+    public interface IRepositoryCreateList<TResult, T>
+    {
+        IList<TResult> CreateList(IList<T> model);
+    }
+
+    public interface IRepositoryCreateList<TResult, TKey1, TKey2>
+    {
+        IList<TResult> CreateList(IList<TKey1> key1, TKey2 key2);
+    }
+
+}
