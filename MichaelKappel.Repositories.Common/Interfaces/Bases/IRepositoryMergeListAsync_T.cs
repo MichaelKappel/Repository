717776a46ab99acd327dbc946905af5f3ebbb175
Michael Kappel <mike@antichrist.net>
2024-10-27 07:42:26 -0700

    Untested added Embeddings

---
+++ MichaelKappel.Repositories.Common/Interfaces/Bases/IRepositoryMergeListAsync_T.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MichaelKappel.Repository.Interfaces.Bases
+{
+    public interface IRepositoryMergeListAsync<T, TUnsaved>
+    {
+        Task<IList<T>> MergeListAsync(IList<TUnsaved> models);
+    }
+
+    public interface IRepositoryMergeListAsync<TResult, TUnsaved, TKey2>
+    {
+        Task<IList<TResult>> MergeListAsync(IList<TUnsaved> models, TKey2 key2);
+    }
+
+    public interface IRepositoryMergeListAsync<TResult, TUnsaved, TKey2, TKey3>
+    {
+        Task<IList<TResult>> MergeListAsync(IList<TUnsaved> models, TKey2 key2, TKey3 key3);
+    }
+
+    public interface IRepositoryMergeListAsync<TResult, TUnsaved, TKey2, TKey3, TKey4>
+    {
+        Task<IList<TResult>> MergeListAsync(IList<TUnsaved> models, TKey2 key2, TKey3 key3, TKey4 key4);
+    }
+
+}
---
+++ MichaelKappel.Repositories.Common/Interfaces/Bases/IRepositoryMergeList_T.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MichaelKappel.Repository.Interfaces.Bases
+{
+    public interface IRepositoryMergeList<T, TUnsaved>
+    {
+        IList<T> MergeList(IList<TUnsaved> models);
+    }
+
+    public interface IRepositoryMergeList<TResult, TUnsaved, TKey2>
+    {
+        TResult MergeList(IList<TUnsaved> models, TKey2 key2);
+    }
+
+    public interface IRepositoryMergeList<TResult, TUnsaved, TKey2, TKey3>
+    {
+        TResult MergeList(IList<TUnsaved> models, TKey2 key2, TKey3 key3);
+    }
+
+    public interface IRepositoryMergeList<TResult, TUnsaved, TKey2, TKey3, TKey4>
+    {
+        TResult MergeList(IList<TUnsaved> models, TKey2 key2, TKey3 key3, TKey4 key4);
+    }
+
+}
