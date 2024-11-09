e0828b3e59071506c919c52e950a99de033318ad
Michael Kappel <mike@antichrist.net>
2024-11-09 04:50:42 -0800

    WIP

--- MichaelKappel.Repositories.Common/Interfaces/Bases/IRepositoryMergeList_T.cs
+++ MichaelKappel.Repositories.Common/Interfaces/Bases/IRepositoryMergeList_T.cs
@@ -12,17 +12,17 @@ namespace MichaelKappel.Repository.Interfaces.Bases
 
     public interface IRepositoryMergeList<TResult, TUnsaved, TKey2>
     {
-        TResult MergeList(IList<TUnsaved> models, TKey2 key2);
+        IList<TResult> MergeList(IList<TUnsaved> models, TKey2 key2);
     }
 
     public interface IRepositoryMergeList<TResult, TUnsaved, TKey2, TKey3>
     {
-        TResult MergeList(IList<TUnsaved> models, TKey2 key2, TKey3 key3);
+        IList<TResult> MergeList(IList<TUnsaved> models, TKey2 key2, TKey3 key3);
     }
 
     public interface IRepositoryMergeList<TResult, TUnsaved, TKey2, TKey3, TKey4>
     {
-        TResult MergeList(IList<TUnsaved> models, TKey2 key2, TKey3 key3, TKey4 key4);
+        IList<TResult> MergeList(IList<TUnsaved> models, TKey2 key2, TKey3 key3, TKey4 key4);
     }
 
 }
